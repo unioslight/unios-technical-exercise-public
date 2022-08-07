28146025c467c1f309613e58f8f54be480fc740a
mp4nguyen <mp4nguyen@gmail.com>
2022-08-07 22:03:03 +0800

    baseservice todos

--- src/service/base.service.ts
+++ src/service/base.service.ts
@@ -1,8 +1,15 @@
 import { PrismaClient, Prisma } from '@prisma/client';
 import prisma from 'db/prisma';
 
+/*
+* TODOS: 
+* - implement the log service
+* - create the config folder in /src; then create the config data for each envs: base.ts; dev.ts; stg.ts; prd.ts ... and have a helper to create the config for running env;
+*.  the confg the is set into baseservice      
+*/
 export default class BaseService{
-
+    
+    protected _config;
     protected _prisma = prisma;
     protected _Prisma = Prisma;
     protected _PrismaClient = PrismaClient;         
